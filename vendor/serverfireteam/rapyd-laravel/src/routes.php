c09e357d910ad94f61b24c17e4db7e0fea9f4736
FadIndia <fadindia2015@gmail.com>
2015-12-26 10:18:23 +0530

    vendor removed

--- vendor/serverfireteam/rapyd-laravel/src/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-//Route::get('rapyd-ajax/{hash}', array('as' => 'rapyd.remote', 'uses' => '\Zofe\Rapyd\Controllers\AjaxController@getRemote'));
-//Route::controller('rapyd-demo','\Zofe\Rapyd\Demo\DemoController');
-
