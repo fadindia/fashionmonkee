291dbb5362c34245b5eaa1d6c7e2f85fc8eae478
FadIndia <fadindia2015@gmail.com>
2015-12-31 00:28:20 +0530

    user page updated

--- public/fm_user/src/controllers/user.ctrl.js
+++ public/fm_user/src/controllers/user.ctrl.js
@@ -1,12 +1,48 @@
-angular.module('fashionApp').controller('userCtrl',function($scope,$api){
-    $scope.currentTab='bookmarks';
-    $scope.setTabContent=function(value){
-    	$scope.currentTab=value;
-    };
-     var user=new $api('user/users.json');
-        user.list().then(function(response) {         
-          $scope.user=response.data.user;
+angular.module('fashionApp').controller('userCtrl', function($scope, $api, $stateParams, userInfo, $q, $state) {
+    var loginDetailsChecker = $q.defer();
+    if(userInfo.getCurrentlyLoggedInUser().id === null){
+        var loginDetails = new $api('user/loginDetails.json');
+        loginDetails.list().then(function (response) {
+            userInfo.setCurrentlyLoggedInUser(response.data);
+            loginDetailsChecker.resolve() || loginDetailsChecker;
         });
-  
+    }else{
+        loginDetailsChecker = loginDetailsChecker.resolve() || loginDetailsChecker;
+    }
+    loginDetailsChecker.promise.then(function () {
+        $scope.currentTab = 'followers';
+        $scope.searchingUser = '';
+        $scope.idOfCurrentUser = parseInt($stateParams.userId || 0, 10);
+        $scope.idOfCurrentlyLoggedInUser = parseInt(userInfo.getCurrentlyLoggedInUser().id || 0, 10);
+        $scope.isProfileInEditMode = false;
+        $scope.setTabContent = function(value) {
+            $scope.currentTab = value;
+        };
+        var user = new $api('user/users.json?userId=' + $stateParams.userId);
+        user.list().then(function(response) {
+            $scope.user = response.data.user;
+        });
+        var allUsers = new $api('user/allUsers.json?name=contains:' + $scope.searchingUser);
+        /*allUsers.list().then(function(response) {
+            $scope.allUsers = response.data.user;
+        });*/
+        $scope.toggleProfileEditMode = function () {
+            if(!$scope.isProfileInEditMode){
+                $scope.profileEditModel = {"name": $scope.user.profile.name,
+                                            "place": $scope.user.profile.place,
+                                            "mobile": $scope.user.profile.mobile,
+                                            "aboutYourself": $scope.user.profile.aboutYourself};
+                $scope.isProfileInEditMode = true;
+            }else{
+                $scope.isProfileInEditMode = false;
+            }
+        };
+        $scope.followThisUser = function () {
+            //TODO;
+        };
+        $scope.goToUser = function (userId) {
+            $state.go('user', {userId: userId});
+        };
+    });
 });
 
